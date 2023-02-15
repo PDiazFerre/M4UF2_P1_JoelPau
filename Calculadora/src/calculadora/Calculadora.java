9088e741df413370613d2e557a088a52f1d96722
paudc <paudc@MATEBOOK-PADICA>
2023-02-15 20:20:38 +0100

    Comentarios de línia y espaciado del programa para hacerlo mas visual

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -23,45 +23,51 @@ public class Calculadora {
         
         //CODI QUE DEMANA INTRODUIR EL PRIMER NÚMERO
         
+        
         do {
 
             String numero1;
             do {
                 System.out.println("\n Introdueix el primer numero. ");
                 numero1 = sc.nextLine();
+                
             } while (!numero1.matches("[+-]?[\\d]*[.]?[\\d]+"));
             double nume1 = Double.parseDouble(numero1);
             double n1 = new Double(numero1);
             
-            
-            
-        
-        
+                    
         //CODI QUE DEMANA INTRODUIR UN SIGNE PER REALITZAR LA OPERACIÓ
 
             do {
                 System.out.println("\n Operació? (Indica el signe)");
-                System.out.println("+ = sumar \n - = restar \n"
-                        + " x = multiplicar \n / = dividir \n * = elevar primer num al segon num."
+                System.out.println("+ = sumar \n "
+                        + "- = restar \n"
+                        + " x = multiplicar \n "
+                        + "/ = dividir \n "
+                        + "* = elevar primer num al segon num."
                         + "\n % = residu");
                 operacion = sc.nextLine();
                 if (operacion.equals("+") || operacion.equals("-") || operacion.equals("x")
                         || operacion.equals("X") || operacion.equals("/") || operacion.equals("%")
                         || operacion.equals("*")) {
                     comprobar = true;
+                    
                 } else {
                     comprobar = false;
+                    
                 }
             } while (comprobar != true);
             
             
             //CODI QUE DEMANA INTRODUIR UN SEGON NÚMERO
             
-
             String numero2;
+            
             do {
+                
                 System.out.println("\n Introdueix el segon numero.");
                 numero2 = sc.nextLine();
+                
             } while (!numero2.matches("[+-]?[\\d]*[.]?[\\d]+"));
             double nume2 = Double.parseDouble(numero2);
             double n2 = new Double(numero2);
@@ -70,42 +76,57 @@ public class Calculadora {
             //CODI QUE REALITZA LES OPERACIONS
 
             do {
+                
                 comprobar = true;
+                
                 switch (operacion) {
+                    
                     case "+":
                         res = n1 + n2;
                         break;
+                        
                     case "-":
                         res = n1 - n2;
                         break;
+                        
                     case "x":
+                        
                     case "X":
                         res = n1 * n2;
                         break;
+                        
                     case "/":
+                                                
                         while (n2 == 0) {
                             do {
                                 System.err.println(" Al denominador hi ha un zero \n"
                                         + "per a  evitar errors coloca un altre valor.");
                                 numero2 = sc.nextLine();
+                                
                             } while (!numero2.matches("[+-]?[\\d]*[.]?[\\d]+"));
                             nume2 = Double.parseDouble(numero2);
                             n2 = new Double(numero2);
                         }
+                                                                 
                         res = n1 / n2;
                         break;
+                        
                     case "*":
                         res = Math.pow(n1, n2);
                         break;
+                        
                     case "%":
                         while (n2 == 0) {
+                            
                             do {
                                 System.err.println(" Al denominador hi ha un zero \n"
                                         + "per a  evitar errors coloca un altre valor.");
                                 numero2 = sc.nextLine();
+                                
                             } while (!numero2.matches("[+-]?[\\d]*[.]?[\\d]+"));
                             nume2 = Double.parseDouble(numero2);
                             n2 = new Double(numero2);
+                            
                         }
                         res = n1 % n2;
                         break;
@@ -113,14 +134,17 @@ public class Calculadora {
                 
                 
                 
-                //CODI PER A SEGUIR AMB EL PROGRAMA O FINALITZAR-LO
-                
-                
+            //IMPRESION DE RESULTADOS
+            
             } while (comprobar != true);
 
             System.out.println("(" + numero1 + ") " + operacion + " (" + numero2 + ")" + " = " + res);
             System.out.println("\n Vols continuar operant? \n");
             System.out.println(" [s/n]");
+            
+            
+            //CONTINUAR CON EL PROGRAMA O FINALIZAR
+            
             do {
                 comprobar = true;
                 operacion = sc.nextLine();
@@ -140,4 +164,4 @@ public class Calculadora {
     }
 }
 
-//fin del programa
+
